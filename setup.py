c805f76f5ed03b3c7edc81a327b70e6afe1a78e5
Jürgen Eckel <juergen@riddleandcode.com>
2022-09-08 21:30:44 +0200

    adj. the cc dependency
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("CHANGELOG.rst") as changelog_file:
 
 install_requires = [
     "requests>=2.20.0",
-    "planetmint-cryptoconditions>=0.9.0",
+    "planetmint-cryptoconditions>=0.10.0",
     "pysha3~=1.0.2",
     "python-rapidjson~=0.6.0",
     "python-rapidjson-schema==0.1.1",
